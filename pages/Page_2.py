bcf2397cc088f03a482bf271a6439244e85296f6
Tan Chengyi <337247014@qq.com>
2025-07-29 06:07:53 +0000

    build a multiple page app structure

---
+++ pages/Page_2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Page 2", page_icon="📈")
+st.sidebar.header("Page 2 Demo")
+
+st.markdown("# Page 2 ")
+
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+st.sidebar.success("Select a demo above.")
+
+
+st.title("🎈 My new app ")
+st.write("# Welcome to Streamlit! 👋")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
